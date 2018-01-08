307283a57d42272ab008763ec78d3b726d77c8e7
baileyschena <baileyschena@gmail.com>
2018-01-08 16:18:47 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 
 bot.on('ready', () => {
- bot.log(`Logged in as ${bot.user.tag}!`);
+ console.log(`Logged in as ${bot.user.tag}!`);
 });
 
 bot.on('message', msg => {
