4995656599c85755bbe74beaba63d5ca16202323
unknown <baileyschena@gmail.com>
2018-01-10 20:13:43 -0500

    fixed again holy shit

--- bot.js
+++ bot.js
@@ -20,18 +20,6 @@ bot.on('message', msg => {
     msg.reply('git works on chromebook idiot');
   }
 });
-if (command === "quote") {
 
-  let quote = args[0];
-  if (isNaN(quote)) return;
-  let msg = await message.channel.fetchMessage(quote);
-  if (!msg) return;
-  let embed = new Discord.RichEmbed()
-  .setAuthor(msg.author.username, msg.author.avatarURL)
-  .setDescription(msg.content)
-  .setFooter(`Quoted by ${message.author.tag} | Sent by ${msg.author.tag} at ${moment(msg.createdTimestamp).format("MMMM Do YYYY, h:mm:ss a")} in ${msg.channel.recipient.username}'s DM`)
-  .setColor(1303248)
-  message.reply({ embed });
-}
 
 bot.login(process.env.BOT_TOKEN);
