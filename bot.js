f167129d9721510f1caa452d7fba93826fd0b495
unknown <baileyschena@gmail.com>
2018-01-10 20:44:32 -0500

    fix3

--- bot.js
+++ bot.js
@@ -2,15 +2,13 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 const prefix = "!"
 
-if(message.content.indexOf(prefix) !== 0) return;
 
 bot.on('ready', () => {
  console.log(`Logged in as ${bot.user.tag}!`);
 });
 
 bot.on('message', msg => {
-  if (message.content.startsWith(prefix + "ping") {
-    message.reply("Pong!") {
+  if (msg.content.startsWith(prefix + "ping") {
     msg.reply('Pong!');
   }
 });
