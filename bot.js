43dcbab8f66b605eddd8d528cb83d77ca5f54697
unknown <baileyschena@gmail.com>
2018-01-11 20:33:32 -0500

    avatar

--- bot.js
+++ bot.js
@@ -11,6 +11,7 @@ bot.on('ready', () => {
 bot.on('message', msg => {
   let command = msg.content.split(" ")[0];
     command = command.slice(botsettings.prefix.length);
+    let args = message.content.split(" ")[0]
     if (!msg.content.startsWith(botsettings.prefix)) return;
     if (command === "pingg") {
       msg.reply("pong");
@@ -34,8 +35,11 @@ bot.on('message', msg => {
       msg.reply("I'm sorry I had to send this to you but now that you have opened it you can't stop reading this. Hi my name is Teresa Fidalgo I died 27 years. If you don't send this to 20 people I will sleep by your side forever. If you don't believe me search me up. Teresa Fidalgo. So send this to 20 people. A girl ignored this and her mom died 20 days later. NO SEND BACKS!!!!! #copied sorry to send this. Btw this is not fake search her up on google : )");
     }
     if (command === "avatar") {
-      msg.channel.send(msg.author.avatarURL);
+      if (!args[0]) return;
+      if (!message.mentions.members.first()) return;
+      msg.channel.send(members.mentions.first().user.avatarURL);
     }
+
 });
 
 
